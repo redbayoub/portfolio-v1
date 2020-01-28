f48fc5fa5da0245647ba3bd44aa0bb93ca9c5591
Red Ayoub <redayoub47@gmail.com>
2020-01-28 17:51:42 +0100

    added vue i18n plugin

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
+  i18n,
   render: function (h) { return h(App) }
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   "transpileDependencies": [
-  ]
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
+  }
 }
