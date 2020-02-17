2b7b221981d195bab41f411abf48e2b5d588408a
Red Ayoub <redayoub47@gmail.com>
2020-02-17 23:01:15 +0100

    fix contact form issues with netlify by prerendring

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,31 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 module.exports = {
-  "transpileDependencies": [
-  ],
+  transpileDependencies: [],
+  configureWebpack: () => {
+    if (process.env.NODE_ENV !== "production") return;
+    return {
+      plugins: [
+        new PrerenderSPAPlugin(
+          // Absolute path to compiled SPA
+          path.resolve(__dirname, "dist"),
+          // List of routes to prerender
+          ["/ar/","/en/"],
+          {
+            // options
+          }
+        )
+      ]
+    };
+  },
 
   pluginOptions: {
     i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
       enableInSFC: true
     }
   }
-}
+};
