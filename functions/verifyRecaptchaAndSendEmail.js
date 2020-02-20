57e839c74d175b4788499911784df5f972a25c3c
Red Ayoub <redayoub47@gmail.com>
2020-02-20 11:09:28 +0100

    log body verify recaptcha function

--- functions/verifyRecaptchaAndSendEmail.js
+++ functions/verifyRecaptchaAndSendEmail.js
@@ -48,20 +48,7 @@ exports.handler = async (event, context, callback) => {
   }
 
   let body = event.body;
-  // process the urlencoded body of the form submit and put it in a 
-  // map structure
-  let parts = body.split('&');
-  let result = [];
-  // grab the params 
-  for (let i = 0, len = parts.length; i < len; i++) { 
-     let kVal = parts[i].split('=');
-     // replace the + space then decode
-     let key = decodeURIComponent(kVal[0].replace(/\+/g, ' '));
-     result[key] = decodeURIComponent(kVal[1].replace(/\+/g, ' '));
-  }
-  // its always a good idea to log so that we can inspect the params
-  // later in Amazon Cloudwatch
-  console.log(result);
+  console.log(body);
 
   // IMPORTANT
   // When the method is POST, the name will no longer be in the event’s
